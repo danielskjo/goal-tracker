3dd62ee7a7bcf12f0095c1bb25fa50c98b453201
danielskjo <sungkeun.jo@gmail.com>
2022-01-04 17:46:56 -0800

    Setup models and app

---
+++ server/src/__init__.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, redirect
+from flask_jwt_extended import JWTManager
+import os
+
+from .auth import auth
+from .constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from .database import db
+from .goals import goals, Goal
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_mapping(SECRET_KEY=os.environ.get("SECRET_KEY"),
+                                SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+                                SQLALCHEMY_TRACK_MODIFICATIONS=False,
+                                JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'))
+    else:
+        app.config.from_mapping(test_config)
+
+    db.app = app
+    db.init_app(app)
+
+    # @app.get('/')
+    # def home():
+    #     return 'Hello world'
+
+    JWTManager(app)
+
+    app.register_blueprint(auth)
+    app.register_blueprint(goals)
+
+    @app.errorhandler(HTTP_404_NOT_FOUND)
+    def handle_404(e):
+        return jsonify({'error': 'Not found'}), HTTP_404_NOT_FOUND
+
+    @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+    def handle_500(e):
+        return jsonify({'error': 'Something went wrong, we are working on it'}), HTTP_500_INTERNAL_SERVER_ERROR
+
+    return app
---
+++ server/src/database.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.Text(), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+    goals = db.relationship('Goal', backref='user')
+
+    def __repr__(self):
+        return f'User>>>{self.name}\nEmail>>>{self.email}'
+
+
+class Goal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    date = db.Column(db.DateTime, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+
+    def __repr__(self):
+        return f'Goal>>>{self.title}\n{self.description}'
