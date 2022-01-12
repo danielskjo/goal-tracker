c960c5e484b28be65e85680f5a7d5243dd7b32ef
danielskjo <sungkeun.jo@gmail.com>
2022-01-12 15:13:25 -0800

    Added login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = () => {
+        fetch('/api/auth/login', {
+            method: 'POST',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'email': email,
+                'password': password
+            })
+        }).then(res => {
+            console.log(res)
+        }).catch(err => console.log(err))
+    }
+
     return (
-        <div>
-            Login Page
+         <div>
+            <h1>Login</h1>
+            <div>
+                <input type='text' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type='text' placeholder='password' value={password}
+                       onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={handleSubmit}>Login</button>
+            </div>
         </div>
     )
 }
