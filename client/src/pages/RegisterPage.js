a2ef2fd030843622bbf00f28a9e205bf235e02e3
danielskjo <sungkeun.jo@gmail.com>
2022-01-12 15:10:31 -0800

    Added register page

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const RegisterPage = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = () => {
+        fetch('/api/auth/register', {
+            method: 'POST',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'name': name,
+                'email': email,
+                'password': password
+            })
+        }).then(res => {
+            console.log(res)
+        }).catch(err => console.log(err))
+    }
+
     return (
         <div>
-            Register Page
+            <h1>Register</h1>
+            <div>
+                <input type='text' placeholder='name' value={name} onChange={(e) => setName(e.target.value)}/>
+                <input type='text' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type='text' placeholder='password' value={password}
+                       onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={handleSubmit}>Register</button>
+            </div>
         </div>
     )
 }
