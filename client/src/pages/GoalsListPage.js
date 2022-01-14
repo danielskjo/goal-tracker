0d64c5f64bca8ecfb1c46710ca608f24f0301cbf
danielskjo <sungkeun.jo@gmail.com>
2022-01-13 18:20:15 -0800

    Connected goals api to frontend

--- client/src/pages/GoalsListPage.js
+++ client/src/pages/GoalsListPage.js
@@ -1,33 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const GoalsListPage = () => {
-    // let [goals, setGoals] = useState([])
-    //
-    // useEffect(() => {
-    //     getGoals()
-    // }, [])
-    //
-    // let getGoals = async () => {
-    //     let response = await fetch('/api/goals/')
-    //     let data = await response.json()
-    //     setGoals(data["data"])
-    // }
+    let [goals, setGoals] = useState([])
+    const token = sessionStorage.getItem('token')
+
+    useEffect(() => {
+        getGoals()
+    }, [])
+
+    let getGoals = async () => {
+        console.log(token);
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        })
+
+        fetch('/api/goals/', {
+            method: 'GET',
+            headers: headers,
+        }).then(res => {
+            if (res.status === 200) return res.json()
+            else alert('There has been some error')
+        }).then(data => {
+            console.log('This came from the server', data)
+            setGoals(data.data)
+        }).catch(err => console.log('There was an error', err))
+    }
 
     return (
         <div>
-            {/*<div>*/}
-            {/*    <h1>Goals (Count: {goals.length}</h1>*/}
-            {/*</div>*/}
-            {/*<div>*/}
-            {/*    {goals.map((goal, index) => (*/}
-            {/*        <div key={index}>*/}
-            {/*            <h3>{goal["title"]}</h3>*/}
-            {/*            <p>{goal["description"]}</p>*/}
-            {/*            <p>{goal["date"]}</p>*/}
-            {/*        </div>*/}
-            {/*    ))}*/}
-            {/*</div>*/}
-            Goals List Page
+            {token && token !== '' && token !== undefined ?
+                (<div>
+                    <h1>Goals (Count: {goals.length})</h1>
+                    <div>
+                        {goals.map((goal, index) => (
+                            <div key={index}>
+                                <h3>{goal["title"]}</h3>
+                                <p>{goal["description"]}</p>
+                                <p>{goal["date"]}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>) :
+                'Goals List Page'
+            }
         </div>
     )
 }
