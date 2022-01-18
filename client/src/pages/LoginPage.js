57fdae520fdcac5af77892b8420ad570ac4c659e
danielskjo <sungkeun.jo@gmail.com>
2022-01-17 16:47:00 -0800

    Added styles to login and register page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,14 +1,23 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {Navigate, useNavigate} from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
 
 const LoginPage = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
     const token = sessionStorage.getItem('token')
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const formData = new FormData(e.currentTarget)
 
         fetch('/api/auth/login', {
             method: 'POST',
@@ -16,8 +25,8 @@ const LoginPage = () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                'email': email,
-                'password': password
+                'email': formData.get('email'),
+                'password': formData.get('password')
             })
         }).then(res => {
             if (res.status === 200) return res.json()
@@ -34,13 +43,61 @@ const LoginPage = () => {
         <div>
             {token && token !== '' && token !== undefined ?
                 <Navigate to='/'/> :
-                <div>
-                    <h1>Login</h1>
-                    <input type='text' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-                    <input type='text' placeholder='password' value={password}
-                           onChange={(e) => setPassword(e.target.value)}/>
-                    <button onClick={handleSubmit}>Login</button>
-                </div>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline/>
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                            <LockOutlinedIcon/>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Login
+                        </Typography>
+                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{mt: 3, mb: 2}}
+                            >
+                                Login
+                            </Button>
+                            <Grid container>
+                                <Grid item>
+                                    <Link href="/register" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Container>
             }
         </div>
     )
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,15 +1,23 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {Navigate, useNavigate} from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const RegisterPage = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
     const token = sessionStorage.getItem('token')
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const formData = new FormData(e.currentTarget)
 
         fetch('/api/auth/register', {
             method: 'POST',
@@ -17,9 +25,9 @@ const RegisterPage = () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                'name': name,
-                'email': email,
-                'password': password
+                'name': formData.get('name'),
+                'email': formData.get('email'),
+                'password': formData.get('password')
             })
         }).then(res => {
             if (res.status === 201) return res.json()
@@ -36,14 +44,71 @@ const RegisterPage = () => {
         <div>
             {token && token !== '' && token !== undefined ?
                 <Navigate to='/'/> :
-                <div>
-                    <h1>Register</h1>
-                    <input type='text' placeholder='name' value={name} onChange={(e) => setName(e.target.value)}/>
-                    <input type='text' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-                    <input type='text' placeholder='password' value={password}
-                           onChange={(e) => setPassword(e.target.value)}/>
-                    <button onClick={handleSubmit}>Register</button>
-                </div>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline/>
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                            <LockOutlinedIcon/>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Register
+                        </Typography>
+                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="name"
+                                label="Name"
+                                name="name"
+                                autoComplete="name"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{mt: 3, mb: 2}}
+                            >
+                                Register
+                            </Button>
+                            <Grid container>
+                                <Grid item>
+                                    <Link href="/login" variant="body2">
+                                        {"Already have an account? Sign In"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Container>
             }
         </div>
     )
