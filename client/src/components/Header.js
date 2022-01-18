2b44353d4d91c16dd29862febdbc3ba0c0ea6cc4
danielskjo <sungkeun.jo@gmail.com>
2022-01-17 16:31:05 -0800

    Created header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,30 +1,68 @@
 import React from 'react'
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import FlagIcon from '@mui/icons-material/Flag';
 
 
 const Header = () => {
     const token = sessionStorage.getItem('token')
     const navigate = useNavigate();
 
-    const handleLogout = (e) => {
-        e.preventDefault();
-        sessionStorage.removeItem('token')
+    const navigateToHome = () => {
+        navigate('')
+        window.location.reload()
+    }
+
+
+    const navigateToRegister = () => {
+        navigate('register')
+        window.location.reload()
+    }
+
+    const navigateToLogin = () => {
         navigate('login')
         window.location.reload()
     }
 
+    const handleLogout = (e) => {
+        e.preventDefault();
+        sessionStorage.removeItem('token')
+        navigate('')
+        window.location.reload()
+    }
+
     return (
-        <div className="app-header">
-            <Link to="/">Home</Link>
-            {token && token !== '' && token !== undefined ?
-                <button onClick={handleLogout}>Logout</button> :
-                (<div>
-                    <Link to="/register">Register</Link>
-                    {" "}
-                    <Link to="/login">Login</Link>
-                </div>)
-            }
-        </div>
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{mr: 2}}
+                    >
+                        <FlagIcon/>
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        <Button color="inherit" onClick={navigateToHome}>Home</Button>
+                    </Typography>
+                    {token && token !== '' && token !== undefined ?
+                        <Button color="inherit" onClick={handleLogout}>Logout</Button> :
+                        (<div>
+                            <Button color="inherit" onClick={navigateToRegister}>Register</Button>
+                            {" "}
+                            <Button color="inherit" onClick={navigateToLogin}>Login</Button>
+                        </div>)
+                    }
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
